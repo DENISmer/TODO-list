c31cdb0cbd4be34baf21968dee9bc63242e0285a
redis <rediskink@inbox.ru>
2023-04-27 19:55:45 +0700

    - работает добавление списка и его обновление - подправил передачу пропсов

--- src/ModalWindow/ModalWindow.jsx
+++ src/ModalWindow/ModalWindow.jsx
@@ -3,18 +3,20 @@ import ModalWinStyle from './ModalWindow.module.css'
 import closeIcon from '../icons/closeButton/1x/twotone_close_black_24dp.png'
 export function ModalWindow(props){
     const [isActive,setIsActive] = useState(false)
-
-
+    const [textAreaValue,setTextAreaValue] = useState('asd')
+    const handle = (data)=> {
+        console.log(data)
+    }
     return(<>{props.isActive &&
 
         <div className={ModalWinStyle.ModalMainStyle}>
             <div className={ModalWinStyle.Window}>
                 <h3>Write new TODO</h3>
                 <div className={ModalWinStyle.CloseButton} onClick={props.isActivepdate(props.isActive)}><img src={closeIcon} /></div>
-                <textarea />
+                <textarea onChange={(e) =>setTextAreaValue(e.target.value)} value={textAreaValue}/>
 
                 <div>
-                    <button>confirm</button>
+                    <button onClick={() => props.addNewLi(textAreaValue)}>confirm</button>
                 </div>
             </div>
 
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,13 +1,24 @@
 import React, {useState} from "react";
 import HeaderStyle from './Header.module.css'
 import {ModalWindow} from "../ModalWindow/ModalWindow";
+import {List} from "../List/List";
 
 export function Header(){
     const [isActive,setIstActive] = useState(false);
     const [list,setList] = useState([])
+    const [li,setLi] = useState([])
+    let localData = [];
     const handle = (value) =>{
         setIstActive(!value)
         console.log(isActive)
+    }
+    const addNewLi = (text) =>{
+        if(text !== undefined){
+            localData.push({txt: text,value: text})
+        }
+        setLi(localData)
+        console.log(localData)
+
     }
     const UpdateToDoValue = (value) =>{
         setIstActive(!value)
@@ -16,8 +27,14 @@ export function Header(){
     return(<>
         <div className={HeaderStyle.Header} onClick={() => handle(isActive)}>
             <h1>Click to add new</h1>
+
         </div>
-            <ModalWindow isActive={isActive} isActivepdate={() => handle}/>
+            <div>
+                {li.map((li,index)=>(
+                    <List key={index} value={li.txt} />
+                ))}
+            </div>
+            <ModalWindow isActive={isActive} isActivepdate={() => handle} addNewLi={addNewLi}/>
         </>
     );
 }
